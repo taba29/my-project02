58241d9181b76a2a96aeea7bc83c556d7f3fbe1e
taba29 <210627686+taba29@users.noreply.github.com>
2026-05-23 11:31:33 +0900

    feat: add PartyScene

---
+++ Assets/_Project/Script/system/PartyState.cs
@@ -0,0 +1,8 @@
+public static class PartyState
+{
+    public static string monsterName = "スライム";
+    public static int level = 5;
+
+    public static int maxHP = 30;
+    public static int currentHP = 18;
+}

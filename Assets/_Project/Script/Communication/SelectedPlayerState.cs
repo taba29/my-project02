b7e92de00775f367058aded204740482e7c4132b
taba29 <210627686+taba29@users.noreply.github.com>
2026-07-04 19:26:02 +0900

    Add selected player state for communication

---
+++ Assets/_Project/Script/Communication/SelectedPlayerState.cs
@@ -0,0 +1,14 @@
+public static class SelectedPlayerState
+{
+    public static string playerName;
+
+    public static int level;
+    public static int victoryCount;
+    public static int defeatCount;
+
+    public static int currentHP;
+    public static int maxHP;
+
+    public static int exp;
+    public static int nextLevelExp;
+}
